e348ec260783dce3cf291e03b3f5ac82af11e6e6
Bob Morane <killian.murphy@telecom-sudparis.eu>
2019-08-01 17:05:26 +0200

    add normalization and train-test split

--- torch_model.py
+++ torch_model.py
@@ -8,7 +8,7 @@ from torch.autograd import Variable
 import torchvision
 import torchvision.transforms as transforms
 import os
-from sklearn import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler
 
 import pandas as pd
 
@@ -25,6 +25,21 @@ print("Here we goooooo !")
 train_batch_size = 42
 test_batch_size = 42
 
-scale = MinMaxScaler()
-X = scale(X)
+scaler = MinMaxScaler()
+X = scaler.transform(X)
+
+features_train, features_test, targets_train, targets_test = train_test_split(X,Y,test_size=0.2, random_state=42)
+
+X_train = torch.from_numpy(features_train)
+X_test = torch.from_numpy(features_test)
+
+Y_train = torch.from_numpy(targets_train).type(torch.LongTensor) 
+Y_test = torch.from_numpy(targets_test).type(torch.LongTensor)
+
+train = torch.utils.data.TensorDataset(X_train,Y_train)
+test = torch.utils.data.TensorDataset(X_test,Y_test)
+
+train_loader = torch.utils.data.DataLoader(train, batch_size = train_batch_size, shuffle = False)
+test_loader = torch.utils.data.DataLoader(test, batch_size = test_batch_size, shuffle = False)
+
 
