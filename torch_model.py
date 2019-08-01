dde1ad9ee7c136b42686f0a07f886cc1f09cadf7
Bob Morane <killian.murphy@telecom-sudparis.eu>
2019-08-01 17:07:25 +0200

    Change to fit interactive python

--- torch_model.py
+++ torch_model.py
@@ -1,3 +1,4 @@
+# %%
 #!/bin/python3
 import numpy as np
 import pandas as pd
@@ -12,16 +13,19 @@ from sklearn.preprocessing import MinMaxScaler
 
 import pandas as pd
 
+# %%
 mnist = sklearn.datasets.fetch_mldata('MNIST original', data_home='datasets/')
 
 y = pd.Series(mnist.target).astype('int').astype('category')
 X = pd.DataFrame(mnist.data)
 
+# %%
 print(y.value_counts())
 print(X.describe())
 
 print("Here we goooooo !")
 
+# %%
 train_batch_size = 42
 test_batch_size = 42
 
@@ -30,6 +34,7 @@ X = scaler.transform(X)
 
 features_train, features_test, targets_train, targets_test = train_test_split(X,Y,test_size=0.2, random_state=42)
 
+# %%
 X_train = torch.from_numpy(features_train)
 X_test = torch.from_numpy(features_test)
 
@@ -42,4 +47,5 @@ test = torch.utils.data.TensorDataset(X_test,Y_test)
 train_loader = torch.utils.data.DataLoader(train, batch_size = train_batch_size, shuffle = False)
 test_loader = torch.utils.data.DataLoader(test, batch_size = test_batch_size, shuffle = False)
 
+# %%
 
